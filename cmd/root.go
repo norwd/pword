137bf2d30502bb65b4ff3d36563fe55f3213b469
norwd <106889957+norwd@users.noreply.github.com>
2022-10-13 09:53:25 +1300

    Re-implement main using cobra front end

---
+++ cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+const version = "v0.0.0"
+
+var rootCmd = &cobra.Command{
+	Use: "pword",
+	Short: "A small, command line password generator.",
+	Long: "Generate passwords for any occasion, using a variety of backends.",
+	Version: version,
+	CompletionOptions: cobra.CompletionOptions {
+		HiddenDefaultCmd: true,
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ cmd/simple.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"crypto/rand"
+	"encoding/base64"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	simpleCmd.Flags().IntP("length", "l", 16, "How long the password should be.")
+	rootCmd.AddCommand(simpleCmd)
+}
+
+var simpleCmd = &cobra.Command{
+	Use: "simple",
+	Short: "Generates a simple password.",
+	Long: "Generate a simple base64 encoded password from random data.",
+	RunE: runSimpleCmd,
+}
+
+func runSimpleCmd(cmd *cobra.Command, args []string) (err error) {
+	var length int
+
+	if length, err = cmd.Flags().GetInt("length"); err != nil {
+		return
+	}
+
+	buf := make([]byte, length)
+
+	if _, err = rand.Read(buf[:]); err != nil {
+		return
+	}
+
+	fmt.Println(base64.RawStdEncoding.EncodeToString(buf[:])[:length])
+
+	return
+}
+
