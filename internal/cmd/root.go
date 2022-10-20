7792672bec347f09c2db855c52b1715060ff372d
norwd <norwd@users.noreply.github.com>
2022-10-20 21:45:04 +0000

    Auto go-fmt

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -9,9 +9,9 @@ import (
 const version = "v0.0.0"
 
 var rootCmd = &cobra.Command{
-	Use: "pword",
-	Short: "A small, command line password generator.",
-	Long: "Generate passwords for any occasion, using a variety of backends.",
+	Use:     "pword",
+	Short:   "A small, command line password generator.",
+	Long:    "Generate passwords for any occasion, using a variety of backends.",
 	Version: version,
 	CompletionOptions: cobra.CompletionOptions{
 		HiddenDefaultCmd: true,
