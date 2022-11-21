66f38c4357cda1608f4b45c5c29847eb7fa42965
norwd <106889957+norwd@users.noreply.github.com>
2022-11-22 11:19:29 +1300

    Test for empty character classes

--- internal/dat/chars_test.go
+++ internal/dat/chars_test.go
@@ -6,10 +6,23 @@ import (
 	"testing"
 )
 
-func TestAllCharacterClassesAreComplete(t *testing.T) {
+func TestAllCharacterClassesAreProvided(t *testing.T) {
 	for name, class := range CharacterClasses {
 		name := name
 		class := class
+
+		t.Run(name, func(t *testing.T) {
+			if len(class) == 0 {
+				t.Errorf("Empty character class %q", name)
+			}
+		})
+	}
+}
+
+func TestAllCharacterClassesAreComplete(t *testing.T) {
+	for name, class := range CharacterClasses {
+		name := name
+		class := string(class)
 
 		t.Run(name, func(t *testing.T) {
 			regex, err := regexp.Compile("[[:" + name + ":]]")
@@ -18,10 +31,10 @@ func TestAllCharacterClassesAreComplete(t *testing.T) {
 				t.Fatalf("Could not compile regex: %q", err)
 			}
 
-			matches := regex.FindAllString(string(class), -1)
+			matches := regex.FindAllString(class, -1)
 
-			if want, have := string(class), strings.Join(matches, ""); want != have {
-				t.Errorf("Not all listed characters are matched by class: want %q but have %q", want, have)
+			if have := strings.Join(matches, ""); class != have {
+				t.Errorf("Not all listed characters are matched by class: want %q but have %q", class, have)
 			}
 		})
 	}
